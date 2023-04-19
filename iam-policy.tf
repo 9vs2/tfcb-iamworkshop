d0671b78c1e0eac2a25a28f5bbe9f219fdbfc786
구제서 <jsgu@koreanair.com>
2023-04-19 17:22:28 +0900

    iam commit

--- iam-policy.tf
+++ iam-policy.tf
@@ -3,14 +3,5 @@ resource "aws_iam_policy" "policy" {
   path        = "/"
   description = "My test policy"
 
-  policy = templatefile("policy.json.tftpl", {
-    aws_account_id = data.aws_caller_identity.current.account_id
-    aws_region = var.aws_region,
-    function_name = var.function_name
-  })
-}
-
-resource "aws_iam_policy" "policy_for_iam" {
-  name = "policy_${var.service_name}"
-  policy = templatefile("policy.json.tftpl")
+  policy = templatefile("policy.json.tftpl",)
 }
