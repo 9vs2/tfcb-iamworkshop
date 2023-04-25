c3cdd720d69bd8fc1adefe95f6829c6578ce7fbe
구제서 <jsgu@koreanair.com>
2023-04-25 17:31:04 +0900

    ec2 commit

--- lambda-jsgu/webserver_ec2.tf
+++ lambda-jsgu/webserver_ec2.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "webserver" {
     Name = "webserver"
   }
 
-  depends_on = [data.terraform_remote_state.vpc.aws_internet_gateway.igw]
+#   depends_on = [data.terraform_remote_state.vpc.aws_internet_gateway.igw]
 }
 
 resource "aws_security_group" "webserversg" {
